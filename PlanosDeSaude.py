09cf7b91a8ce9421909ea71a2aee8b20e390c222
Tiago Souza <tiagosouzas.contato@gmail.com>
2026-08-27 12:52:30 -0300

    Atualiza lógica do plano  de saúde e padroniza variáveis

--- PlanosDeSaude.py
+++ PlanosDeSaude.py
@@ -1,12 +1,12 @@
 print('Bem vindo ao Sistema de desconto Saúde')
 
-ValorBase = float(input('Informe o valor base do plano: R$ '))
+valor_base = float(input('Informe o valor base do plano: R$ '))
 idade = int(input('Informe a idade do cliente: '))
 
 if idade>= 0 and idade <19: 
     desconto = (100/100)
 elif idade>= 19 and idade <29:
-    desconto = (150/150)
+    desconto = (150/100)
 elif idade>= 29 and idade <39: 
     desconto = (225/100)
 elif idade>= 39 and idade <49:
@@ -16,6 +16,6 @@ elif idade >= 49 and idade <59:
 else:
     desconto = (600/100) 
 
-ValorMensal = ValorBase * desconto
+valor_mensal = valor_base * desconto
 
-print(f'O valor mensal do plano é de: R$ {ValorMensal:.2f}')
+print(f'O valor mensal do plano é de: R$ {valor_mensal:.2f}')
